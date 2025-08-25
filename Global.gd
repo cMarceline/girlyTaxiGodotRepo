dc181c00265b8c03aca204ec3ad5c2316d2ee123
Jodie H <ballaticseal@gmail.com>
2025-08-25 22:37:54 +1000

    Working wwise lol

--- Global.gd
+++ Global.gd
@@ -1,1 +1,9 @@
 extends Node
+
+func _ready() -> void:
+	Wwise.register_game_obj(self, self.name)
+	Wwise.register_listener(self)
+	
+	if OS.has_feature("editor"):
+		Wwise.load_bank_id(AK.BANKS.TESTSOUNDBANK)
+		Wwise.post_event_id(AK.EVENTS.PLAYTESTTONE, self)
