fa0e0af86fb1627e52e13a5d38a914e0dfeb17a3
Marcie Collins <marciecollins12@gmail.com>
2025-08-29 02:26:48 +1000

    Wowie we now have the collision Array

--- character_body_3d.gd
+++ character_body_3d.gd
@@ -8,9 +8,11 @@ var goalVelocity : Vector3
 var myGrav : float
 var mySpeed : float
 
-# State Machine for 
+# State Machine for states
 enum States {DRIVING, FLYING, FALLING, FLOATING}
 var state: States
+
+var collisionArray : Array[RayCast3D]
 
 # Steering Input Variables
 var steer : float :
@@ -20,13 +22,41 @@ var steerU : float :
 var lean : float :
 	get : return Input.get_action_raw_strength("leanR") - Input.get_action_raw_strength("leanL")
 
+func _ready() : 
+	if 1 == 0 : return
+
 func _process(delta: float) -> void:
-	1 == 1
+	if 1 == 0 : return
+
+# Integrate forces happens on the step before physics processing
+# Mostly used for collisionArray
+func _integrate_forces(state):
+	# Delete collisions and Clear Array
+	for n in collisionArray.size() :
+		collisionArray[n].queue_free()
+	collisionArray.clear()
+	# Create the collision Array
+	for n in state.get_contact_count() :
+		var contactPos = state.get_contact_collider_position(n) - self.position
+		collisionArray.append(_newRaycast(contactPos*1.1))
+		collisionArray[n].global_rotation = Vector3.ZERO
+
 
 func _physics_process(delta: float) -> void:
-	# Gives scales for all the various input Directions
 	_steerHandler(delta)
 	_stateChecker()
+	
+	# Find the collision normals that best aligns with cars rotation
+	var bestNormal : Vector3 = Vector3.ZERO
+	for n in collisionArray.size() : 
+		collisionArray[n].force_raycast_update()
+		var normal = collisionArray[n].get_collision_normal()
+		var normalDot = normal.dot(transform.basis.y)
+		if normalDot > bestNormal.dot(transform.basis.y) : 
+			bestNormal = normal
+	# Align world to bestNormal
+	if bestNormal != Vector3.ZERO : 
+		print(bestNormal)
 	
 	# Apply Gravity when Falling
 	if state == States.FALLING :
@@ -43,7 +73,7 @@ func _physics_process(delta: float) -> void:
 	# Debug Braking
 	if Input.is_action_pressed("Brake"):
 		myVelocity = Vector3.ZERO
-		
+
 	# Acceleration when Not Floating
 	if Input.is_action_pressed("Accelerate") && state != States.FLOATING:
 		myVelocity += Vector3(0,0,100 * delta)
@@ -57,14 +87,17 @@ func _physics_process(delta: float) -> void:
 		transform.basis.y * myVelocity.y + 
 		transform.basis.z * myVelocity.z
 	)
-	
+
 	linear_velocity = goalVelocity #linear_velocity.lerp(goalVelocity,delta)
 	# Lerp into the forward velocity
 	# Get the normal vector of a collision
 
-#func _integrate_forces(state):
-	#var contact_point = state.get_contact_collider_position(0) - self.position
-	#print(contact_point)
+func _newRaycast( pos : Vector3 ) :
+	var newRaycast = RayCast3D.new()
+	newRaycast.target_position = pos
+	self.add_child(newRaycast)
+	newRaycast.force_raycast_update()
+	return newRaycast
 
 func _steerHandler(delta) : 
 	# Stick Steering
