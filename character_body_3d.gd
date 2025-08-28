cdb877a681277e8cefb63d4f4f538c8e1607d14d
Marcie Collins <marciecollins12@gmail.com>
2025-08-28 22:20:28 +1000

    reorganisation and absolute befuddlement

--- character_body_3d.gd
+++ character_body_3d.gd
@@ -3,9 +3,11 @@ extends RigidBody3D
 var gs: int = 9.8
 
 var myVelocity : Vector3 
+var goalVelocity : Vector3
 
 var myGrav : float
 var mySpeed : float
+var currentVelocity : Vector3
 
 enum States {DRIVING, FLYING, FALLING, FLOATING}
 var state: States
@@ -16,42 +18,44 @@ func _process(delta: float) -> void:
 	#print(state)
 
 func _physics_process(delta: float) -> void:
-	#Rotate around down
+	# Gives scales for all the various input Directions
 	var steer = Input.get_action_raw_strength("SteerL") - Input.get_action_raw_strength("SteerR")
 	var steerU = Input.get_action_raw_strength("SteerU") - Input.get_action_raw_strength("SteerD")
 	var lean = Input.get_action_raw_strength("leanR") - Input.get_action_raw_strength("leanL")
-
-	transform = transform.rotated_local(Vector3(0,1,0),steer * delta)
-
+	_steerHandler(steer, steerU, lean, delta)
+	
+	# Apply Gravity when Falling
 	if state == States.FALLING :
 		myGrav += gs * delta * 10
-		myVelocity.y = -myGrav
 	else : 
 		myGrav = 0.2
-
+	myVelocity.y = -myGrav
+	
+	# Freeze in Float
 	if state == States.FLOATING :
-		transform = transform.rotated_local(Vector3(1,0,0),steerU * delta)
-		transform = transform.rotated_local(Vector3(0,0,1),lean * delta)
 		linear_velocity = Vector3.ZERO
 		return
 
+	# Debug Braking
 	if Input.is_action_pressed("Brake"):
-		linear_velocity = Vector3.ZERO
-		return
+		myVelocity = Vector3.ZERO
+		#return
 		#print("Braking")
 
+	# Acceleration when Not Floating
 	if Input.is_action_pressed("Accelerate") && state != States.FLOATING:
 		myVelocity += Vector3(0,0,100 * delta)
-
+	
+	# Applying Friction
 	elif state == States.DRIVING :
-		myVelocity.z /= 1 + (0.2 * delta)
-		myVelocity.z -= clamp(myVelocity.z * 0.2*delta,0,myVelocity.z)
+		myVelocity.z -= delta / 2 * myVelocity.z
 
 	var goalVelocity : Vector3 = (
 		transform.basis.x * myVelocity.x +
 		transform.basis.y * myVelocity.y + 
 		transform.basis.z * myVelocity.z
 	)
+	
 	linear_velocity = goalVelocity #linear_velocity.lerp(goalVelocity,delta)
 	# Lerp into the forward velocity
 	# Get the normal vector of a collision
@@ -60,13 +64,17 @@ func _physics_process(delta: float) -> void:
 	#print(linear_velocity)
 
 func _integrate_forces(state):
-	var contact_point = state.get_contact_collider_position(0) #- self.position  linear_velocity
+	var contact_point = state.get_contact_collider_position(0) - self.position
 	print(contact_point)
 
 
 
-func _steerHandler() : 
-	print("yep")
+func _steerHandler(steer, steerU, lean, delta) : 
+	# Stick Steering
+	transform = transform.rotated_local(Vector3(0,1,0),steer * delta)
+	if state == States.FLOATING :
+		transform = transform.rotated_local(Vector3(1,0,0),steerU * delta)
+		transform = transform.rotated_local(Vector3(0,0,1),lean * delta)
 
 func _stateChecker():
 	if Input.is_action_pressed("Free"): state = States.FLOATING
